acee3dfeeaa932e1b863882bdd738d33386c0ead
mohamed sbeta <mohamedsbeta@gmail.com>
2019-11-05 09:58:09 +0100

    warmUp15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,39 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+/*function revers(str){
+	var arr  = str.split(' ');
+		console.log(arr);
+		newArr = [];
+		var newStr = "";
+
+	for(var i = 0; i < arr.length; i++){
+			newArr.unshift(arr[i])
+	}
+		console.log(newArr);
+		
+
+		for(var i = 0; i < arr.length; i++) {
+			newStr = newStr + 
+		}
+		
+
+
+		return newArr.toString();
+
+
+
+}*/
+
+
+function revers(str){
+	var newStr = "";
+
+	for(var i = 0; i < str.length; i++){
+		newStr = newStr + str[i];
+	}
+
+
+	return newStr;
+}
