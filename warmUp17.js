92ea782ac950cf045c93ae4d60f63c0417bd7c19
mohamed sbeta <mohamedsbeta@gmail.com>
2019-11-08 09:52:02 +0100

    warmUp17

--- warmUp17.js
+++ warmUp17.js
@@ -1,6 +1,8 @@
 // There is a special keyboard with all keys in a single row.
 // ​
-// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. The time taken to move your finger from index i to index j is |i - j|.
+// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), 
+//initially your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. 
+//The time taken to move your finger from index i to index j is |i - j|.
 // ​
 // You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
 // ​
@@ -27,6 +29,27 @@
 
 // ​
 // ​
-// var calculateTime = function(keyboard, word) {
-    
+ //var calculateTime = function(keyboard, word) {
+
+
 // };
+
+function time(keyboard, word){
+	var count = 0;
+	var count2=0;
+	var keyArr = keyboard.split('');
+	var wordArr = word.split('');
+
+	for (var i = 0; i < keyArr.length; i++){
+		for (var k = 0; k < wordArr.length; k++){
+			if(keyArr[i] === wordArr[k]){
+				count = count + 1;
+			};count2 = count2 + i
+		};
+	};
+	return count2;
+	
+}
+
+/* it dose not work yet
+
