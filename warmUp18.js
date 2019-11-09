783e1a7064e12218130bda0b32b1b02efae62cea
mohamed sbeta <mohamedsbeta@gmail.com>
2019-11-09 09:44:31 +0100

    warmUp18

--- warmUp18.js
+++ warmUp18.js
@@ -1,6 +1,7 @@
 // You are given an input string.
 
-// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+// For each symbol in the string if it's the first character occurence, replace it with a '1',
+// else replace it with the amount of times you've already seen it...
 
 // But will your code be performant enough?
 // Examples:
@@ -10,3 +11,27 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function count(str) {
+var count= 0;
+var result = "";
+
+	for(var i = 0; i < str.length; i++){
+		count = 0
+		for(var k = 0; k < str.length; k++){
+			if(str[i] === str[k]){
+				count+= 1;
+			}
+		 
+		
+	}result = result + count.toString();
+}
+return result;
+}
+
+// not the exact recuirment 
+
+
+
+
+
